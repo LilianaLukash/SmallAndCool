45dbec4c801f2b44cbcd438e7c48bc356eaecb3a
Liliana <liliana.ubisoft@gmail.com>
2023-12-28 01:33:58 +0000

    screenshot works

--- models.py
+++ models.py
@@ -1,35 +1,62 @@
-class ScreenCaptureTool(tk.Tk):
-    def __init__(self):
-        super().__init__()
-        self.start_x = None
-        self.start_y = None
+import tkinter as tk
+from PIL import ImageGrab
+
+class CaptureScreen:
+    def __init__(self, root):
+        self.root = root
+        self.begin_x = None
+        self.begin_y = None
+        self.end_x = None
+        self.end_y = None
+
+        self.selection = None
+        self.start_point = None
         self.rect = None
-        self.deiconify()
-        self.attributes("-fullscreen", True)
-        self.canvas = tk.Canvas(self, cursor="cross", bg="grey", opacity=0.3)
+
+        root.attributes('-fullscreen', True)
+        root.attributes('-alpha', 0.3)  # Прозорість
+
+        self.canvas = tk.Canvas(root, cursor="cross")
         self.canvas.pack(fill="both", expand=True)
+
         self.canvas.bind("<ButtonPress-1>", self.on_button_press)
         self.canvas.bind("<B1-Motion>", self.on_move_press)
         self.canvas.bind("<ButtonRelease-1>", self.on_button_release)
-        self.selected_area = None
 
     def on_button_press(self, event):
-        self.start_x = self.canvas.canvasx(event.x)
-        self.start_y = self.canvas.canvasy(event.y)
+        self.start_point = event
+        self.begin_x = event.x
+        self.begin_y = event.y
+
         if not self.rect:
-            self.rect = self.canvas.create_rectangle(self.start_x, self.start_y, 1, 1, outline="red")
+            self.rect = self.canvas.create_rectangle(self.begin_x, self.begin_y, self.begin_x, self.begin_y, outline='red')
 
     def on_move_press(self, event):
-        cur_x = self.canvas.canvasx(event.x)
-        cur_y = self.canvas.canvasy(event.y)
-        self.canvas.coords(self.rect, self.start_x, self.start_y, cur_x, cur_y)
+        curX, curY = (event.x, event.y)
+
+        if self.rect:
+            self.canvas.coords(self.rect, self.begin_x, self.begin_y, curX, curY)
+
+
 
     def on_button_release(self, event):
-        self.selected_area = (self.start_x, self.start_y, self.canvas.canvasx(event.x), self.canvas.canvasy(event.y))
-        self.capture_selected_area()
-        self.destroy()
+        print("Відпускання кнопки")
+        self.end_x, self.end_y = (event.x, event.y)
+        self.root.quit()
 
-    def capture_selected_area(self):
-        x1, y1, x2, y2 = self.selected_area
-        image = ImageGrab.grab(bbox=(x1, y1, x2, y2))
-        # Тут можна виконати додаткові дії з захопленим зображенням
+def capture_area():
+    root = tk.Tk()
+    capture_screen = CaptureScreen(root)
+    root.mainloop()
+
+    # Координати виділеної області
+    x1, y1, x2, y2 = capture_screen.begin_x, capture_screen.begin_y, capture_screen.end_x, capture_screen.end_y
+
+    # Захоплення скріншоту обраної області
+    screenshot = ImageGrab.grab(bbox=(x1, y1, x2, y2))
+    return screenshot
+
+# Приклад використання
+screenshot = capture_area()
+
+screenshot.show()
